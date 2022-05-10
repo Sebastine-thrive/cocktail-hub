1f926c5646601421383c721d31130547dd5168f2
Sebastine Ogu (Thrive) <sebastinoptimum@gmail.com>
2022-05-10 07:22:07 +0100

    first commit of the cocktails hub files

---
+++ src/pages/About.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        We at cocktailPub provide you with wonderful-looking and more wonderfully-tasting cocktails
+        as provided by Cocktailsdb.com<br></br>Feel free to make your search, get your ingredients and make your cocktail.
+        That way, all doubts are cleared!  &#128521;
+
+
+      </p>
+    </section>
+  );
+}
